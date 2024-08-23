32cc01b5f791d2eb3e583ccdbd40d63886773cc4
Hyeonmine <gusals3158@naver.com>
2024-08-23 09:45:50 +0900

    [파일명변경]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import AssetTable from "./jsx/assetTable";
-import AssetRegister from "./jsx/AssetRegister";
+import AssetRegister from "./jsx/assetCheck/AssetCheck";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { Component } from "react";
 

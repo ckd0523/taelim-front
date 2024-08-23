2abc4b42b58421990f6035a8f7f3742b77d025b5
Hyeonmine <gusals3158@naver.com>
2024-08-23 09:26:10 +0900

    [Router]

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
 import AssetTable from "./jsx/assetTable";
+import AssetRegister from "./jsx/AssetRegister";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Component } from "react";
 
 function App() {
   return (
@@ -8,6 +11,11 @@ function App() {
       <div className="App">
         <h1>Asset Management</h1>
         <AssetTable />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/asset/categories" element={<AssetRegister />}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );

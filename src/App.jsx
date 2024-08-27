ea8f92af87306442730ab694e0abe31a512300ce
name <kydea2240@naver.com>
2024-08-27 14:33:48 +0900

    240827
    commit 전

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./jsx/assetUpdate/Home";
 import AssetRegister from "./jsx/assetCheck/AssetRegister";
+import Regist from "./jsx/assetUpdate/Regist";
 import About from "./jsx/assetUpdate/About";
 import Contact from "./jsx/assetUpdate/Contact";
 import AssetUpdate from "./jsx/assetUpdate/AssetUpdate";
@@ -21,6 +22,9 @@ function App() {
             <Link to="/check">자산등록화면</Link>
           </li>
           <li>
+            <Link to="/regist">자산등록 예시화면</Link>
+          </li>
+          <li>
             <Link to="/about">자산조회예시</Link>
           </li>
           <li>
@@ -36,9 +40,10 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/check" element={<AssetRegister />} />
+          <Route path="/regist" element={<Regist />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/check" element={<AssetRegister />} />
           <Route path="/update" element={<AssetUpdate />} />
           {/* <Route path="/asset/categories" element={<AssetCheck />} />
           <Route path="/asset/register" element={<AssetRegister />} /> */}

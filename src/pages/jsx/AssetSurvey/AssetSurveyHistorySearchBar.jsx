fa2d42a3b756ba742a16024329a5351b0da4f12c
JCY-3958 <w7e43958@naver.com>
2024-09-23 17:30:37 +0900

    2024-09-23 자산 조사 삭제 고침

--- src/pages/jsx/AssetSurvey/AssetSurveyHistorySearchBar.jsx
+++ src/pages/jsx/AssetSurvey/AssetSurveyHistorySearchBar.jsx
@@ -11,76 +11,78 @@ const SearchBar = () => {
 
   return (
     <Row>
-      <Card></Card>
-      <Card>
-        <Card.Body>
-          <Row>
-            <Col lg={2}>
-              <label>회차</label>
-              <InputGroup>
-                <Form.Control
-                  type='number'
-                  name='round'
-                  min='0'
-                >
-                </Form.Control>
-              </InputGroup>
-            </Col>
-            <Col lg={2}>
-              <label>조사자</label>
-              <InputGroup>
-                <Form.Control
-                  type='text'
-                  name='surveyBy'
-                >
-                </Form.Control>
-              </InputGroup>
-            </Col>
-            <Col lg={3}>
-              <label>위치</label>
-              <Select
-                placeholder='위치를 선택하세요'
-                className="react-select"
-                classNamePrefix="react-select"
-                options={assetSurveyLocation}
-              ></Select>
-            </Col>
+      <Col>
+        <Card></Card>
+        <Card>
+          <Card.Body>
+            <Row>
+              <Col lg={2}>
+                <label>회차</label>
+                <InputGroup>
+                  <Form.Control
+                    type='number'
+                    name='round'
+                    min='0'
+                  >
+                  </Form.Control>
+                </InputGroup>
+              </Col>
+              <Col lg={2}>
+                <label>조사자</label>
+                <InputGroup>
+                  <Form.Control
+                    type='text'
+                    name='surveyBy'
+                  >
+                  </Form.Control>
+                </InputGroup>
+              </Col>
+              <Col lg={3}>
+                <label>위치</label>
+                <Select
+                  placeholder='위치를 선택하세요'
+                  className="react-select"
+                  classNamePrefix="react-select"
+                  options={assetSurveyLocation}
+                ></Select>
+              </Col>
 
-            <Col>
-              <Row className='justify-content-start'>
-                <label>자산 조사일</label>
-                <Col lg={4}>
-                  <div className="mb-3">
-                    <CustomDatePicker
-                      hideAddon={true}
-                      value={selectedDate}
-                      onChange={(date) => {
-                        setSelectedDate(date);
-                      }}
-                    />
-                  </div>
-                </Col>
-                ~
-                <Col lg={4}>
-                  <div className="mb-3">
-                    <CustomDatePicker
-                      hideAddon={true}
-                      value={selectedDate}
-                      onChange={(date) => {
-                        setSelectedDate(date);
-                      }}
-                    />
-                  </div>
-                </Col>
+              <Col>
+                <Row className='justify-content-start'>
+                  <label>자산 조사일</label>
+                  <Col lg={4}>
+                    <div className="mb-3">
+                      <CustomDatePicker
+                        hideAddon={true}
+                        value={selectedDate}
+                        onChange={(date) => {
+                          setSelectedDate(date);
+                        }}
+                      />
+                    </div>
+                  </Col>
+                  ~
+                  <Col lg={4}>
+                    <div className="mb-3">
+                      <CustomDatePicker
+                        hideAddon={true}
+                        value={selectedDate}
+                        onChange={(date) => {
+                          setSelectedDate(date);
+                        }}
+                      />
+                    </div>
+                  </Col>
 
-                <Col lg={3}>
-                  <Button>검색</Button>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+                  <Col lg={3}>
+                    <Button>검색</Button>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
     </Row>
   );
 

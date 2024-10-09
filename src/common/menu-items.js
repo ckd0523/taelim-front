7be65be158afbab9991e7de5b5e671fb77f250df
ckd0523 <ckd0523@naver.com>
2024-10-09 18:15:57 +0900

    자산조회 레벨

--- src/common/menu-items.js
+++ src/common/menu-items.js
@@ -77,20 +77,8 @@ const MENU_ITEMS = [
 		icon: ' uil-list-ul',
 		children: [
 			{
-				key: 'ds-AssetPage',
-				label: '자산조회',
-				url: '/jsx/AssetPage',
-				parentKey: 'check',
-			},
-			{
-				key: 'ds-Expand',
-				label: '확장',
-				url: '/jsx/Expand',
-				parentKey: 'check',
-			},
-			{
 				key: 'ds-AssetPageTest',
-				label: '자산조회테스트',
+				label: '자산조회',
 				url: '/jsx/AssetPageTest',
 				parentKey: 'check',
 			},

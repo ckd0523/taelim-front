817f5afd6ef2ad036f55c15e65f75f6ca62bfce9
changhyeon <ckd0523@naver.com>
2024-10-23 16:50:50 +0900

    Update AllChangeModal.jsx

--- src/pages/jsx/Expand/AllChangeModal.jsx
+++ src/pages/jsx/Expand/AllChangeModal.jsx
@@ -2,12 +2,14 @@ import { Row, Col, Card, Button, Modal, Form } from 'react-bootstrap';
 import { useState } from 'react';
 import axios from 'axios';
 import api from '@/common/api/authAxios';
+import { useAuthContext } from '@/common';
 const API_URL = import.meta.env.VITE_BASIC_URL;
 
 const ActionModal = ({ show, handleClose, actionData, actionType }) => {
+	const { user } = useAuthContext();
+
 	const [department, setDepartment] = useState('');
 	const [assetLocation, setAssetLocation] = useState('');
-	const [assetSecurityManager, setAssetSecurityManager] = useState('');
 
 	const [disposeMethod, setDisposeMethod] = useState('');
 	const [disposeLocation, setDisposeLocation] = useState('');
@@ -16,7 +18,6 @@ const ActionModal = ({ show, handleClose, actionData, actionType }) => {
 
 	const departmentChange = (e) => setDepartment(e.target.value);
 	const assetLocationChange = (e) => setAssetLocation(e.target.value);
-	const assetSecurityManagerChange = (e) => setAssetSecurityManager(e.target.value);
 
 	const disposeMethodChange = (e) => setDisposeMethod(e.target.value);
 	const disposeLocationChange = (e) => setDisposeLocation(e.target.value);
@@ -57,15 +58,80 @@ const ActionModal = ({ show, handleClose, actionData, actionType }) => {
 		setOwners([]); // 선택 후 리스트 초기화
 	};
 
+	const [securityManagers, setSecurityManagers] = useState([]);
+	const [assetSecurityManager, setAssetSecurityManager] = useState('');
+	const [assetSecurityManagerID, setAssetSecurityManagerID] = useState('');
+
+	const assetSecurityManagerChange = (e) => {
+		setAssetSecurityManager(e.target.value);
+		// 소유자 검색어에 따라 소유자 검색
+		if (e.target.value) {
+			handleSecurityManagerSearch(e.target.value); // 소유자 검색 호출
+		} else {
+			setSecurityManagers([]); // 입력이 비었으면 리스트 초기화
+		}
+	};
+
+	// 소유자 검색 함수
+	const handleSecurityManagerSearch = async (searchTerm) => {
+		try {
+			const response = await api.get(`${API_URL}/user/search`, {
+				params: { name: searchTerm },
+			});
+			setSecurityManagers(response.data); // 소유자 리스트 업데이트
+		} catch (error) {
+			console.error('소유자 검색 중 오류 발생:', error);
+		}
+	};
+	// 소유자 선택 핸들러
+	const handleSelectSecurityManager = (securityManager) => {
+		setAssetSecurityManager(securityManager.fullname); // 선택된 소유자 이름 설정
+		setAssetSecurityManagerID(securityManager.id);
+		setSecurityManagers([]); // 선택 후 리스트 초기화
+	};
+
+	const [users, setUsers] = useState([]);
+	const [assetUser, setAssetUser] = useState('');
+	const [assetUserID, setAssetUserID] = useState('');
+	const assetUserChange = (e) => {
+		setAssetUser(e.target.value);
+		// 소유자 검색어에 따라 소유자 검색
+		if (e.target.value) {
+			handleUserSearch(e.target.value); // 소유자 검색 호출
+		} else {
+			setUsers([]); // 입력이 비었으면 리스트 초기화
+		}
+	};
+
+	// 소유자 검색 함수
+	const handleUserSearch = async (searchTerm) => {
+		try {
+			const response = await api.get(`${API_URL}/user/search`, {
+				params: { name: searchTerm },
+			});
+			setUsers(response.data); // 소유자 리스트 업데이트
+		} catch (error) {
+			console.error('소유자 검색 중 오류 발생:', error);
+		}
+	};
+	// 소유자 선택 핸들러
+	const handleSelectUser = (user) => {
+		setAssetUser(user.fullname); // 선택된 소유자 이름 설정
+		setAssetUserID(user.id);
+		setUsers([]); // 선택 후 리스트 초기화
+	};
+
 	const handleFormSubmit = () => {
 		const updateToSend = {
 			assetDtos: actionData,
 			department,
 			assetLocation,
-			assetSecurityManager,
+			assetUser: assetUserID,
+			assetSecurityManager: assetSecurityManagerID,
 			assetOwner: assetOwnerID,
 			reason,
 			detail,
+			demandBy: user.id,
 		};
 
 		const disposeToSend = {
@@ -74,12 +140,13 @@ const ActionModal = ({ show, handleClose, actionData, actionType }) => {
 			disposeLocation,
 			reason,
 			detail,
+			demandBy: user.id,
 		};
 
 		const handleSuccessResponse = (response) => {
 			console.log('Success:', response.data);
 			alert(response.data); // 성공 메시지 띄우기
-			window.location.reload(); // 2초 뒤 새로고침
+			//window.location.reload(); // 2초 뒤 새로고침
 		};
 
 		const handleErrorResponse = (error) => {
@@ -99,6 +166,7 @@ const ActionModal = ({ show, handleClose, actionData, actionType }) => {
 					.catch(handleErrorResponse);
 				break;
 			case 'AllUpdateDemand':
+				console.log('확인용', updateToSend);
 				api.post(`${API_URL}/allUpdateDemand`, updateToSend)
 					.then(handleSuccessResponse)
 					.catch(handleErrorResponse);
@@ -157,15 +225,6 @@ const ActionModal = ({ show, handleClose, actionData, actionType }) => {
 									/>
 								</Form.Group>
 
-								<Form.Group className="mb-1">
-									<Form.Label className="mb-0">관리자</Form.Label>
-									<Form.Control
-										type="text"
-										value={assetSecurityManager}
-										onChange={assetSecurityManagerChange}
-									/>
-								</Form.Group>
-
 								{/* 추가된 부분: 소유자 입력 필드 */}
 								<Form.Group className="mb-1">
 									<Form.Label className="mb-0">소유자</Form.Label>
@@ -182,6 +241,55 @@ const ActionModal = ({ show, handleClose, actionData, actionType }) => {
 													onClick={() => handleSelectOwner(owner)}
 												>
 													{owner.fullname} ({owner.department}){' '}
+													{/* 소유자 이름과 부서 */}
+												</li>
+											))}
+										</ul>
+									)}
+								</Form.Group>
+
+								{/* 추가된 부분: 소유자 입력 필드 */}
+								<Form.Group className="mb-1">
+									<Form.Label className="mb-0">사용자</Form.Label>
+									<Form.Control
+										type="text"
+										value={assetUser}
+										onChange={assetUserChange} // 소유자 변경 핸들러
+									/>
+									{users.length > 0 && ( // 소유자 리스트 표시
+										<ul className="owner-list">
+											{users.map((user) => (
+												<li
+													key={user.id}
+													onClick={() => handleSelectUser(user)}
+												>
+													{user.fullname} ({user.department}){' '}
+													{/* 소유자 이름과 부서 */}
+												</li>
+											))}
+										</ul>
+									)}
+								</Form.Group>
+
+								{/* 추가된 부분: 소유자 입력 필드 */}
+								<Form.Group className="mb-1">
+									<Form.Label className="mb-0">보안담당자</Form.Label>
+									<Form.Control
+										type="text"
+										value={assetSecurityManager}
+										onChange={assetSecurityManagerChange} // 소유자 변경 핸들러
+									/>
+									{securityManagers.length > 0 && ( // 소유자 리스트 표시
+										<ul className="owner-list">
+											{securityManagers.map((securityManager) => (
+												<li
+													key={securityManager.id}
+													onClick={() =>
+														handleSelectSecurityManager(securityManager)
+													}
+												>
+													{securityManager.fullname} (
+													{securityManager.department}){' '}
 													{/* 소유자 이름과 부서 */}
 												</li>
 											))}

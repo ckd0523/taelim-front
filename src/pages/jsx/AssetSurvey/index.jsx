6112eb0b06e336a7ca6c1469ee2409ab0b12591a
changhyeon <ckd0523@naver.com>
2024-11-11 15:18:22 +0900

    Update index.jsx

--- src/pages/jsx/AssetSurvey/index.jsx
+++ src/pages/jsx/AssetSurvey/index.jsx
@@ -1,6 +1,3 @@
 export { AssetSurveyHistory2 as default } from './AssetSurveyHistory2';
-<<<<<<< HEAD
 //export { default as AssetSurveyDetail } from './AssetSurveyDetail';
-=======
 export { default as AssetSurveyDetail } from './AssetSurveyDetail';
->>>>>>> marge2
